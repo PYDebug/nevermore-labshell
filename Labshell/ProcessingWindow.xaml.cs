055563b10df7fd060c493785ebafcd3bcb051a22
PanYan <panyan5617998@163.com>
2016-01-09 22:54:48 +0800

    加入摄像头

--- Labshell/ProcessingWindow.xaml.cs
+++ Labshell/ProcessingWindow.xaml.cs
@@ -12,6 +12,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using Labshell.Model;
+using AForge.Video.DirectShow;
+using System.ComponentModel;
+using AForge.Video;
+using System.Drawing;
 
 namespace Labshell
 {
@@ -21,11 +25,13 @@ namespace Labshell
     public partial class ProcessingWindow : Window
     {
         private List<UploadFile> upfiles = new List<UploadFile>();
+        private VideoCaptureDevice device;
 
         public ProcessingWindow()
         {
             InitializeComponent();
             initData();
+            this.Loaded += new RoutedEventHandler(Window_Loaded);
         }
 
         private void initData()
@@ -39,6 +45,8 @@ namespace Labshell
 
         private void CloseButton_Click(object sender, System.Windows.RoutedEventArgs e)
         {
+            device.SignalToStop();
+            device.WaitForStop(); 
             Application.Current.Shutdown();
         }
 
@@ -63,7 +71,32 @@ namespace Labshell
 
         private void ExitButton_Click(object sender, System.Windows.RoutedEventArgs e)
         {
+            device.SignalToStop();
+            device.WaitForStop(); 
             Application.Current.Shutdown();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // 设定初始视频设备  
+            FilterInfoCollection videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            if (videoDevices.Count > 0)
+            {   // 默认设备  
+                device = new VideoCaptureDevice(videoDevices[0].MonikerString);
+                device.NewFrame += new NewFrameEventHandler(videoSourcePlayer_NewFrame);
+                device.DesiredFrameSize = new System.Drawing.Size(160, 120);
+                device.DesiredFrameRate = 1;
+                device.Start();
+            }
+        }
+
+        private void videoSourcePlayer_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap bitmap = (Bitmap)eventArgs.Frame.Clone();
+            
+            GC.Collect();
+
+            picture.Image = bitmap;
         }
     }
 }
